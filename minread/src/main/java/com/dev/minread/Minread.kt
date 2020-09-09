6ea7af50c8b3b9ba21dd29ae1ed74fd5053f7ac3
oppongjr <oppong751@gmail.com>
2020-09-09 11:03:52 +0000

    Refactored function to return minutes read. Closes #2

--- minread/src/main/java/com/dev/minread/Minread.kt
+++ minread/src/main/java/com/dev/minread/Minread.kt
@@ -2,26 +2,15 @@ package com.dev.minread
 
 object Minread {
 
-    fun minutesRead(string: String): String {
+    fun minutesRead(string: String, message: String = "min read"): String {
 
-        val word_count = countWords(string)
-        val total = word_count / 200
-        if (total < 1)
-            return "0.5 min read"
-        else
-            return "$total min read"
+        val wordCount = countWords(string)
+        var minutes = wordCount / 200
+        minutes = if(minutes < 1) 1 else minutes
+
+        return "$minutes $message"
     }
 
-    fun custome_minread(string: String, message: String): String {
-
-        val word_count = countWords(string)
-        val total = word_count / 200
-        if (total < 1)
-            return "0.5 $message"
-        else
-            return "$total $message"
-
-    }
 
     internal fun countWords(inputString: String): Int =
         inputString.trim().split("\\s+".toRegex()).size
